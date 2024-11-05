e33af8ae7edb0838e713695bc2fbff71fc2e58b8
MOHAMED SHEBIN T P <148629731+shebin-zeo@users.noreply.github.com>
2024-11-05 23:36:47 +0530

    Update MergeStringAlter.py

--- MergeStringAlter.py
+++ MergeStringAlter.py
@@ -20,3 +20,32 @@ public:
         
     }
 };
+
+
+// Full C++ Code 
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string word1="1234";
+    string word2="5678";
+    string result;
+    int i=0;
+    while(i < word1.size() || i < word2.size())
+    {
+        if(i < word1.size())
+        {
+            result +=word1[i];
+
+        }
+        if(i < word2.size())
+        {
+            result +=word2[i];
+        }
+        i++;
+    }
+    cout<< "The merged alternative is "<<result;
+}
